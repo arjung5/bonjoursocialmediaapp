d4822882aba2b0f82267b63b1ce9d92242d7396c
arjung5 <arjungargniec@gmail.com>
2021-07-29 15:10:18 +0530

    Changd code post and home controller to async/await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,35 +1,59 @@
 const Post = require('../models/post');
 const User=require('../models/user');
 
-module.exports.homePage=(req,res)=>{
-    Post.find({})
-       .populate('user')
-       .populate(({
-           path:'comments',
-           populate:{
-               path:'user'
-           }
-       })).exec((err,dataPost)=>{
-        if(err)
-        {
-            console.log(`There is error in getting post list : ${err}`);
-            return;
-        }
-        else
-        {
-            //console.log(dataPost);
-            User.find({},(err,data)=>{
-                if(err)
-                {
-                    console.log(`There is error in fetching the list of users : ${err}`);
-                    return;
-                }
-                return res.render('home',{
-                    title:"HOME PAGE",
-                    users_list:data,
-                    post_List:dataPost
-                });
-            })
-        }        
-    });
+module.exports.homePage= async (req,res)=>{
+    //Coverting the code to async await beacause we send flash messages to user and reduces code reading complexity
+    // Post.find({})
+    //    .populate('user')
+    //    .populate(({
+    //        path:'comments',
+    //        populate:{
+    //            path:'user'
+    //        }
+    //    })).exec((err,dataPost)=>{
+    //     if(err)
+    //     {
+    //         console.log(`There is error in getting post list : ${err}`);
+    //         return;
+    //     }
+    //     else
+    //     {
+    //         //console.log(dataPost);
+    //         User.find({},(err,data)=>{
+    //             if(err)
+    //             {
+    //                 console.log(`There is error in fetching the list of users : ${err}`);
+    //                 return;
+    //             }
+    //             return res.render('home',{
+    //                 title:"HOME PAGE",
+    //                 users_list:data,
+    //                 post_List:dataPost
+    //             });
+    //         })
+    //     }        
+    // });
+
+
+
+    //async await version of code
+    try{
+        let post =await Post.find({}).populate('user').populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+        let user=await User.find({});
+        return res.render('home',{
+                            title:"HOME PAGE",
+                            users_list:user,
+                            post_List:post
+                        });
+    }catch(ex)
+    {
+        cosole.log(`There is error coming in reading data for home page${err}`);
+        return;
+    }
+   
 }
--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -2,25 +2,39 @@ const Post=require('../models/post');
 const User = require('../models/user');
 const Comment=require('../models/comment');
 
-module.exports.createPost=(req,res)=>{
+module.exports.createPost=async (req,res)=>{
     if(req.isAuthenticated())
     {
-     
-       Post.create({
-           content:req.body.content,
-           user:req.user.id
-       },(err,postData)=>{
-           if(err)
-           {
-               cnosole.log(`There is error in creating ${err}`);
-               return;
-           }
-           else
-           {
-               console.log(postData);
-               return res.redirect('back');
-           }
-       })
+     try
+     {
+        let post=await Post.create({
+            content:req.body.content,
+            user:req.user.id
+       });
+       return res.redirect('back');
+     }
+     catch(err)
+     {
+         console.log(`Error in creating post and the error is : ${err}`);
+     }
+    
+    
+    //converting code to async/await as above
+    //    Post.create({
+    //        content:req.body.content,
+    //        user:req.user.id
+    //    },(err,postData)=>{
+    //        if(err)
+    //        {
+    //            cnosole.log(`There is error in creating ${err}`);
+    //            return;
+    //        }
+    //        else
+    //        {
+    //            console.log(postData);
+    //            return res.redirect('back');
+    //        }
+    //    })
     }
     else
     {
@@ -28,59 +42,115 @@ module.exports.createPost=(req,res)=>{
     }
 };
 
-module.exports.createComment=(req,res)=>{
+module.exports.createComment=async (req,res)=>{
+  
+    //Converted below callabck writter code to async await approch
+    // Post.findById(req.body.postId,(err,post)=>{
+    //     if(err)
+    //     {
+    //         console.log(`There is some error in fetching data from post ${err}`);
+    //         return;
+    //     }
+    //     else
+    //     {
+    //         Comment.create({
+    //             postId:req.body.postId,
+    //             content:req.body.content,
+    //             user:req.user.id
+    //         },(err, commentData)=>{
+    //             if(err)
+    //             {
+    //                 cnosole.log(`There is error in creating Comment ${err}`);
+    //                 return;
+    //             }
+    //             else
+    //             {
+    //                 post.comments.push(commentData);
+    //                 post.save();
+    //                 return res.redirect('back');
+    //             }
+    //         })
+    //     }
+    // })
 
-    Post.findById(req.body.postId,(err,post)=>{
-        if(err)
+    if(req.isAuthenticated())
+    {
+        try
         {
-            console.log(`There is some error in fetching data from post ${err}`);
+            let post=await Post.findById(req.body.postId);
+            let createCommet=await Comment.create({
+                                     postId:req.body.postId,
+                                     content:req.body.content,
+                                     user:req.user.id
+                                     });
+            console.log(`This is value of Post ${post}`);
+            post.comments.push(createCommet);
+            post.save();
+            return res.redirect('back');
+        }
+        catch(err)
+        {
+            console.log(`This is the error while creating post ${err}`);
             return;
         }
-        else
-        {
-            Comment.create({
-                postId:req.body.postId,
-                content:req.body.content,
-                user:req.user.id
-            },(err, commentData)=>{
-                if(err)
-                {
-                    cnosole.log(`There is error in creating Comment ${err}`);
-                    return;
-                }
-                else
-                {
-                    post.comments.push(commentData);
-                    post.save();
-                    return res.redirect('back');
-                }
-            })
-        }
-    })
+    }
+    else
+    {
+        return res.redirect('back');
+    }
 };
 
-module.exports.destroyPost=(req,res)=>{
+module.exports.destroyPost=async (req,res)=>{
 
-    Post.findById(req.params.postId,(err,success)=>{
-        if(err)
+    if(req.isAuthenticated())
+    {
+        try
         {
-            console.log(`here is the error about the ${err}`)
+            let post=await Post.findById(req.params.postId);
+            if(post.user==req.user.id)
+            {
+                post.remove();
+                let commentDelete=await Comment.deleteMany({post:req.params.id});
+                return res.redirect('/')
+            }
+            else
+            {
+                return res.redirect('/')
+            }
+        }
+        catch(err)
+        {
+            console.log(`This is the error while deleting post ${err}`);
             return;
         }
-        //.id means c 
-        if(success.user==req.user.id)
-        {
-           success.remove();
-           //console.log(`here is desired post ${success}`);
-           Comment.deleteMany({post:req.params.id},(err)=>{
-               if(err)
-               {
-                   console.log(`${err}`)
-                   return;
-               }
-               //console.log(`here is comment delete ${success}`);
-               return res.redirect('back');
-           })
-        }
-    })
+    }
+    else
+    {
+        return res.redirect('/')
+    }
+    
+
+    //converting below wriitern correct code in async/await for btter udenstanding 
+    // Post.findById(req.params.postId,(err,success)=>{
+    //     if(err)
+    //     {
+    //         console.log(`here is the error about the ${err}`)
+    //         return;
+    //     }
+    //     //.id means c 
+    //     if(success.user==req.user.id)
+    //     {
+    //        success.remove();
+    //        //console.log(`here is desired post ${success}`);
+    //        Comment.deleteMany({post:req.params.id},(err)=>{
+    //            if(err)
+    //            {
+    //                console.log(`${err}`)
+    //                return;
+    //            }
+    //            //console.log(`here is comment delete ${success}`);
+    //            return res.redirect('back');
+    //        })
+    //     }
+    // })
 }
